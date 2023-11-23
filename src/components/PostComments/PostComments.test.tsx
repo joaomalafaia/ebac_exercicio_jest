b6217a9d0d09df7476a118358c418cee0387b8f1
João Malafaia <j.malafaia02@gmail.com>
2023-11-23 11:13:36 -0300

    testes adicionados

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,28 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+describe('teste para o componente PostComment', () => {
+    test('deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('deve adicionar dois comentários', () => {
+        render(<PostComment />)
+        fireEvent.change(screen.getByTestId('txt-comentario'), {
+            target: {
+                value: 'teste 1'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+
+        fireEvent.change(screen.getByTestId('txt-comentario'), {
+            target: {
+                value: 'teste 2'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+
+        expect(screen.getByTestId('comentario')).toHaveLength(2)
+    })
 });
